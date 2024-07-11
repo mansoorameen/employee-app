986289f5ec1c7c724fd0e6fad29cd7b1270c4bd2
mansoor <manzooravail@gmail.com>
2024-07-12 02:07:33 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>EmployeeApp</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
